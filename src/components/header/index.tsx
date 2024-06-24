2859b2c63378a6f02dc77dda81351cc607a0311a
Jhon Henao <henao.jhon@correounivalle.edu.co>
2024-06-24 06:59:46 -0500

    Header component was modified. Its width and bg color

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,12 @@
 const Header = () => {
   return (
-    <div className="bg-sky-700 grid grid-flow-col items-center w-full px-6 py-6">
-      <img src="./slack-logo.jpg" alt="logo" width={160} height={80} />
-      <h1 className="text-slate-200 font-semibold text-2xl">
+    <div className="bg-sky-700 grid grid-flow-col items-center w-full p-6">
+      <img
+        src="./slack-logo.jpg"
+        className="col-span-1 max-w-36 h-fit justify-self-center md:col-span-2"
+        alt="logo"
+      />
+      <h1 className="text-slate-200 text-center font-semibold text-2xl col-span-4 md:text-3xl">
         Staff development and training
       </h1>
     </div>
