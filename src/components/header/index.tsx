04b145c0b73ef6125a1dbed3566b60cc35b1cf29
Jhon Henao <henao.jhon@correounivalle.edu.co>
2024-06-24 06:26:09 -0500

    Creating react app

---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="bg-sky-700 grid grid-flow-col items-center w-full px-6 py-6">
+      <img src="./slack-logo.jpg" alt="logo" width={160} height={80} />
+      <h1 className="text-slate-200 font-semibold text-2xl">
+        Staff development and training
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/header";
+
+const Home = () => {
+  return (
+    <div className="flex">
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
