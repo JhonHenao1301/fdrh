a55b5ea63731c5ea620fe2529548d7f70c2386c9
Henao <J.Henao@cgiar.org>
2024-06-26 06:45:31 -0500

    Change the sizes of the header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,16 @@
 const Header = () => {
   return (
-    <div className="bg-sky-700 grid grid-flow-col items-center w-full p-6">
-      <img
-        src="./slack-logo.jpg"
-        className="col-span-1 max-w-36 h-fit justify-self-center md:col-span-2"
-        alt="logo"
-      />
-      <h1 className="text-slate-200 text-center font-semibold text-2xl col-span-4 md:text-3xl">
-        Staff development and training
-      </h1>
+    <div className="bg-sky-700 w-full">
+      <div className="flex justify-between items-center max-w-3xl p-4 header">
+        <img
+          src="./Alliance_logo_standard_white.png"
+          className="col-span-1 w-36 h-fit justify-self-center md:col-span-2 md:w-44"
+          alt="logo"
+        />
+        <h1 className="text-slate-200 text-center font-semibold text-2xl col-span-4 md:text-3xl">
+          Staff development and training
+        </h1>
+      </div>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <div className="flex">
+    <div className="">
       <Header />
     </div>
   );
