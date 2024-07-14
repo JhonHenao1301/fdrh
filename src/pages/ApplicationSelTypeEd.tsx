498bf7d5805fbc590b38b16edfd5192e3ba35b4c
Henao <J.Henao@cgiar.org>
2024-07-13 21:25:32 -0500

    feat:add checkboxes in application page 2

--- src/pages/ApplicationSelTypeEd.tsx
+++ src/pages/ApplicationSelTypeEd.tsx
@@ -8,51 +8,91 @@ const ApplicationSelTypeEd = () => {
         <h1 className="text-lg text-center font-bold">Requester information</h1>
         <section className="grid grid-cols-2 gap-4">
           <article className="flex flex-col justify-between">
-            <h1 className="font-semibold">Name:</h1>
-            <p className="font-semibold text-gray-30">Info</p>
+            <p className="text-sm text-gray-30">Name:</p>
+            <p className="font-semibold">Info</p>
           </article>
           <article className="flex flex-col justify-between ">
-            <h1 className="font-semibold">Resource ID:</h1>
-            <p className="font-semibold text-gray-30">{id}</p>
+            <p className="text-sm text-gray-30">Resource ID:</p>
+            <p className="font-semibold">{id}</p>
           </article>
           <article className="flex flex-col justify-between ">
-            <h1 className="font-semibold">Job title</h1>
-            <p className="font-semibold text-gray-30">Info</p>
+            <p className="text-sm text-gray-30">Job title</p>
+            <p className="font-semibold">Info</p>
           </article>
           <article className="flex flex-col justify-between ">
-            <h1 className="font-semibold">Department or unit:</h1>
-            <p className="font-semibold text-gray-30">Info</p>
+            <p className="text-sm text-gray-30">Department or unit:</p>
+            <p className="font-semibold">Info</p>
           </article>
           <article className="flex flex-col justify-between ">
-            <h1 className="font-semibold">Office</h1>
-            <p className="font-semibold text-gray-30">Info</p>
+            <p className="text-sm text-gray-30">Office</p>
+            <p className="font-semibold">Info</p>
           </article>
           <article className="flex flex-col justify-between ">
-            <h1 className="font-semibold">Duty post:</h1>
-            <p className="font-semibold text-gray-30">Info</p>
+            <p className="text-sm text-gray-30">Duty post:</p>
+            <p className="font-semibold">Info</p>
           </article>
           <article className="flex flex-col justify-between ">
-            <h1 className="font-semibold">Supervisor:</h1>
-            <p className="font-semibold text-gray-30">Info</p>
+            <p className="text-sm text-gray-30">Supervisor:</p>
+            <p className="font-semibold">Info</p>
           </article>
           <article className="flex flex-col justify-between ">
-            <h1 className="font-semibold">Start date:</h1>
-            <p className="font-semibold text-gray-30">Info</p>
+            <p className="text-sm text-gray-30">Start date:</p>
+            <p className="font-semibold">Info</p>
           </article>
           <article className="flex flex-col justify-between ">
-            <h1 className="font-semibold">
+            <p className="text-sm text-gray-30">
               Performance management rating (last year):
-            </h1>
-            <p className="font-semibold text-gray-30">Info</p>
+            </p>
+            <p className="font-semibold">Info</p>
           </article>
         </section>
       </div>
       <form className="flex flex-col gap-6" action="submit">
         <h1 className="font-bold">Education level</h1>
-        <section>
-          Options
-          {/* <Checkbox /> */}
-        </section>
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center">
+            <input
+              id="undergraduate"
+              type="radio"
+              name="drone"
+              value="undergraduate"
+              className="mr-4"
+            />
+            <label htmlFor="undergraduate">Undergraduate</label>
+          </div>
+          <div className="flex items-center">
+            <input
+              id="professional"
+              type="radio"
+              name="drone"
+              value="professional"
+              className="mr-4"
+            />
+            <label htmlFor="professional">
+              Professional Certificate/Specializations
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              id="master"
+              type="radio"
+              name="drone"
+              value="master"
+              className="mr-4"
+            />
+            <label htmlFor="master">Master</label>
+          </div>
+          <div className="flex items-center">
+            <input
+              id="phd"
+              type="radio"
+              name="drone"
+              value="phd"
+              className="mr-4"
+            />
+            <label htmlFor="phd">Phd</label>
+          </div>
+        </div>
         <button className="btn">Next</button>
       </form>
     </div>
