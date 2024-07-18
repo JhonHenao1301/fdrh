03606085fc54db0eea2f50147102445aaa567376
Henao <J.Henao@cgiar.org>
2024-07-17 20:05:10 -0500

    feat: Add all texts for the main form

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -4,9 +4,11 @@ import { initReactI18next } from "react-i18next";
 const resources = {
   en: {
     translation: {
+      // Home
       home_msg1: "Main page",
       home_msg2: "Admin page",
       home_msg3: "Application page",
+      // Main page
       application_msg1: "Staff development and training",
       application_msg2:
         "The Alliance of Bioversity International and CIAT, recognizes that its staff are fundamental to its success. As a research organization, it is essential that the Alliance promotes a strong learning culture in which all employees can develop and learn the competencies and skills required to perform their jobs safely at all times, reach their full potential and meet the evolving needs of the organization.",
@@ -45,12 +47,16 @@ const resources = {
       application_profile_msg8: "Supervisor:",
       application_profile_msg9: "Start date:",
       application_profile_msg10: "Performance management rating (last year):",
+
+      // Application page 2
       application_2_msg1: "Education level",
       application_2_msg2: "Undergraduate",
       application_2_msg3: "Professional Certificate/Specializations",
       application_2_msg4: "Master",
       application_2_msg5: "PhD",
       application_2_msg6: "Next",
+
+      // Application page 3(main)
       application_3_msg1: "Education program information",
       application_3_msg2: "Academic program title:",
       application_3_msg3: "Education level:",
@@ -72,16 +78,31 @@ const resources = {
         "Number of academic periods to carry out during this year:",
       application_3_msg17:
         "Study leave (paid time off to complete the course):",
-      application_3_msg18: "Current status:",
-      application_3_msg19: "",
-      application_3_msg20: "",
+      application_3_msg18: "Current status",
+      application_3_msg19: "Already studying",
+      application_3_msg20: "Admitted but not started",
+      application_3_msg21: "Not applied yet",
+      application_3_msg22:
+        "Start date of the academic program for this year (can be estimated)",
+      application_3_msg23: "Degree year of the academic program (estimated)",
+      application_3_msg24:
+        "Include the 100% of the total cost of the academic periods taken this year US$ (without decimal points or comma",
+      application_3_msg25:
+        "Do you / Will you receive additional financial support or allowances from the Alliance to cover this educational program?",
+      application_3_msg26: "Yes",
+      application_3_msg27: "No",
+      application_3_msg28:
+        "If yes, please indicate the percentage or amount of support per academic period during this year",
     },
   },
   es: {
     translation: {
+      // Página home
       home_msg1: "Página principal",
       home_msg2: "Página de administración",
       home_msg3: "Página de aplicación",
+
+      //Página principal
       application_msg1: "Solicitud de apoyo para capacitación",
       application_msg2:
         "La Alianza de Bioversity Internacional y el CIAT, reconoce que su personal es fundamental para su éxito. Como organización de investigación, es fundamental que la Alianza promueva una sólida cultura de aprendizaje en la que todos los empleados puedan desarrollar y aprender las competencias y habilidades necesarias para realizar su trabajo de manera segura en todo momento, alcanzar su máximo potencial y satisfacer las necesidades cambiantes de la organización.",
@@ -110,6 +131,8 @@ const resources = {
       application_msg16: "Siguiente",
       application_msg17: "Limpiar",
       application_msg18: "Introduce un valor válido",
+
+      // Página con el perfil del user
       application_profile_msg1: "Información del solicitante",
       application_profile_msg2: "Nombre:",
       application_profile_msg3: "Carnet:",
@@ -121,38 +144,52 @@ const resources = {
       application_profile_msg9: "Fecha de inicio:",
       application_profile_msg10:
         "Resultado de la gestión de desempeño (año pasado):",
+
+      //Aplicación página 2
       application_2_msg1: "Nivel educativo",
       application_2_msg2: "Carrera Universitaria",
       application_2_msg3: "Especialización",
       application_2_msg4: "Maestría",
       application_2_msg5: "Doctorado",
       application_2_msg6: "Siguiente",
+
+      // Formulario principal
       application_3_msg1: "Información del programa educativo",
       application_3_msg2: "Programa Académico",
-      application_3_msg3: "Nivel educativo:",
+      application_3_msg3: "Nivel educativo",
       application_3_msg4:
         "Cómo se relaciona este estudio con los objetivos de la Organización y de su área/unidad?:",
       application_3_msg5: "Modalidad",
       application_3_msg6: "Presencial",
       application_3_msg7: "Semi-presencial",
       application_3_msg8: "Virtual",
-      application_3_msg9: "Nombre de la Universidad o Institución académica:",
-      application_3_msg10: "Periodo por el que se solicita este apoyo:",
+      application_3_msg9: "Nombre de la Universidad o Institución académica",
+      application_3_msg10: "Periodo por el que se solicita este apoyo",
       application_3_msg11:
-        "Duración total del programa académico (Número total de períodos académicos: semestres, módulos, etc.):",
+        "Duración total del programa académico (Número total de períodos académicos: semestres, módulos, etc.)",
       application_3_msg12: "Descripción",
       application_3_msg13: "Semestres",
       application_3_msg14: "Períodos",
       application_3_msg15: "Módulos",
       application_3_msg16:
-        "Número de periodos académicos a realizar durante este año:",
+        "Número de periodos académicos a realizar durante este año",
       application_3_msg17:
-        "Licencia de estudio (tiempo libre pagado para completar el curso):",
-      application_3_msg18: "Estado actual:",
-      application_3_msg19: "",
-      application_3_msg20: "",
-      application_3_msg21:
-        "Fecha de inicio del programa académico para este año (puede ser estimado):",
+        "Licencia de estudio (tiempo libre pagado para completar el curso)",
+      application_3_msg18: "Estado actual",
+      application_3_msg19: "Se encuentra estudiando",
+      application_3_msg20: "Ya fue admitido, no inicia aún",
+      application_3_msg21: "No se ha postulado",
+      application_3_msg22:
+        "Año de grado del programa académico(puede ser estimado)",
+      application_3_msg23:
+        "Incluir el 100% del costo total de los períodos académicos a realizarse durante este año US$ (sin puntos o comas)",
+      application_3_msg24:
+        "Recibe o recibirá algún otro apoyo financiero por parte de la Alianza (unidad o proyecto) para cubrir este programa educativo?",
+      application_3_msg25: "Si",
+      application_3_msg26: "No",
+      application_3_msg27: "Si",
+      application_3_msg28:
+        "En caso afirmativo, indique el porcentaje o la cantidad de apoyo por período académico durante este año:",
     },
   },
 };
--- src/pages/ApplicationForm.tsx
+++ src/pages/ApplicationForm.tsx
@@ -120,10 +120,16 @@ const ApplicationForm = () => {
             )}
           </div>
           <div className="flex flex-col gap-2">
-            <label htmlFor="institution">{t("application_3_msg9")}</label>
+            <label
+              htmlFor="institution"
+              className="text-gray-30 text-sm text-start transition-all"
+            >
+              {t("application_3_msg9")}
+            </label>
             <input
               type="text"
-              className="text-gray-30 text-sm text-start transition-all"
+              className="border-b p-2 border-b-primary-10 rounded-md focus:outline-none focus:border-primary-30 focus:border-b-2 transition-colors"
+              autoComplete="off"
               {...register("institution", {
                 required: {
                   value: true,
@@ -138,10 +144,16 @@ const ApplicationForm = () => {
             )}
           </div>
           <div className="flex flex-col gap-2">
-            <label htmlFor="period">{t("application_3_msg10")}</label>
+            <label
+              htmlFor="period"
+              className="text-gray-30 text-sm text-start transition-all"
+            >
+              {t("application_3_msg10")}
+            </label>
             <input
               type="text"
-              className="text-gray-30 text-sm text-start transition-all"
+              className="border-b p-2 border-b-primary-10 rounded-md focus:outline-none focus:border-primary-30 focus:border-b-2 transition-colors"
+              autoComplete="off"
               {...register("period", {
                 required: {
                   value: true,
@@ -156,14 +168,23 @@ const ApplicationForm = () => {
             )}
           </div>
           <div className="flex gap-4 justify-between">
-            <label className="text-start w-1/2" htmlFor="program">
+            <label
+              className="text-gray-30 text-sm text-start transition-all"
+              htmlFor="program"
+            >
               {t("application_3_msg11")}
             </label>
             <div className="flex gap-2 items-center w-1/2">
-              <label htmlFor="numPeriods">#</label>
+              <label
+                htmlFor="numPeriods"
+                className="text-gray-30 text-sm text-start transition-all"
+              >
+                #
+              </label>
               <input
                 type="number"
-                className="bg-primary-10 rounded-md px-2 py-1 w-1/3"
+                className="border-b p-2 border-b-primary-10 rounded-md focus:outline-none focus:border-primary-30 focus:border-b-2 transition-colors"
+                autoComplete="off"
                 {...register("numPeriods", {
                   required: {
                     value: true,
@@ -176,11 +197,17 @@ const ApplicationForm = () => {
                   {errors.numPeriods?.message}
                 </span>
               )}
-              <label htmlFor="typePeriods">{t("application_3_msg12")}</label>
+              <label
+                htmlFor="typePeriods"
+                className="text-gray-30 text-sm text-start transition-all"
+              >
+                {t("application_3_msg12")}
+              </label>
               <select
                 name="typePeriods"
                 id="typePeriods"
-                className="bg-primary-10 rounded-md px-2 py-1 w-2/3"
+                className="border-b p-2 border-b-primary-10 rounded-md focus:outline-none focus:border-primary-30 focus:border-b-2 transition-colors"
+                autoComplete="off"
               >
                 <option value="">{t("application_3_msg13")}</option>
                 <option value="">{t("application_3_msg14")}</option>
@@ -189,12 +216,16 @@ const ApplicationForm = () => {
             </div>
           </div>
           <div className="flex flex-col gap-2">
-            <label className="text-start" htmlFor="academicPeriods">
+            <label
+              className="text-gray-30 text-sm text-start transition-all"
+              htmlFor="academicPeriods"
+            >
               {t("application_3_msg16")}
             </label>
             <input
               type="text"
-              className="text-gray-30 text-sm text-start transition-all"
+              className="border-b p-2 border-b-primary-10 rounded-md focus:outline-none focus:border-primary-30 focus:border-b-2 transition-colors"
+              autoComplete="off"
               {...register("academicPeriods", {
                 required: {
                   value: true,
@@ -209,12 +240,16 @@ const ApplicationForm = () => {
             )}
           </div>
           <div className="flex flex-col gap-2">
-            <label className="text-start" htmlFor="studyLeave">
+            <label
+              className="text-gray-30 text-sm text-start transition-all"
+              htmlFor="studyLeave"
+            >
               {t("application_3_msg17")}
             </label>
             <input
               type="text"
-              className="text-gray-30 text-sm text-start transition-all"
+              className="border-b p-2 border-b-primary-10 rounded-md focus:outline-none focus:border-primary-30 focus:border-b-2 transition-colors"
+              autoComplete="off"
               {...register("studyLeave", {
                 required: {
                   value: true,
@@ -229,16 +264,161 @@ const ApplicationForm = () => {
             )}
           </div>
           <div className="flex flex-col gap-2">
-            <label htmlFor="period">{t("application_3_msg18")}</label>
+            <label
+              className="text-gray-30 text-sm text-start transition-all"
+              htmlFor="period"
+            >
+              {t("application_3_msg18")}
+            </label>
             <select
               name="typePeriods"
               id="typePeriods"
-              className="text-gray-30 text-sm text-start transition-all"
+              className="border-b p-2 border-b-primary-10 rounded-md focus:outline-none focus:border-primary-30 focus:border-b-2 transition-colors"
+              autoComplete="off"
             >
-              <option value="">{t("application_3_msg5")}</option>
-              <option value="">{t("application_3_msg6")}</option>
-              <option value="">{t("application_3_msg")}</option>
+              <option value="">{t("application_3_msg19")}</option>
+              <option value="">{t("application_3_msg20")}</option>
+              <option value="">{t("application_3_msg21")}</option>
             </select>
+          </div>
+          <div className="flex flex-col gap-2">
+            <label
+              className="text-gray-30 text-sm text-start transition-all"
+              htmlFor="startDate"
+            >
+              {t("application_3_msg22")}
+            </label>
+            <input
+              type="text"
+              className="border-b p-2 border-b-primary-10 rounded-md focus:outline-none focus:border-primary-30 focus:border-b-2 transition-colors"
+              autoComplete="off"
+              {...register("startDate", {
+                required: {
+                  value: true,
+                  message: "This field is required",
+                },
+              })}
+            />
+            {errors.startDate && (
+              <span className="text-sm text-red-20 text-start">
+                {errors.startDate?.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label
+              className="text-gray-30 text-sm text-start transition-all"
+              htmlFor="degree"
+            >
+              {t("application_3_msg23")}
+            </label>
+            <input
+              type="text"
+              className="border-b p-2 border-b-primary-10 rounded-md focus:outline-none focus:border-primary-30 focus:border-b-2 transition-colors"
+              autoComplete="off"
+              {...register("degree", {
+                required: {
+                  value: true,
+                  message: "This field is required",
+                },
+              })}
+            />
+            {errors.degree && (
+              <span className="text-sm text-red-20 text-start">
+                {errors.degree?.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label
+              className="text-gray-30 text-sm text-start transition-all"
+              htmlFor="studyLeave"
+            >
+              {t("application_3_msg24")}
+            </label>
+            <input
+              type="text"
+              className="border-b p-2 border-b-primary-10 rounded-md focus:outline-none focus:border-primary-30 focus:border-b-2 transition-colors"
+              autoComplete="off"
+              {...register("studyLeave", {
+                required: {
+                  value: true,
+                  message: "This field is required",
+                },
+              })}
+            />
+            {errors.studyLeave && (
+              <span className="text-sm text-red-20 text-start">
+                {errors.studyLeave?.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label
+              className="text-gray-30 text-sm text-start transition-all"
+              htmlFor="status"
+            >
+              {t("application_3_msg25")}
+            </label>
+            <input
+              type="text"
+              className="border-b p-2 border-b-primary-10 rounded-md focus:outline-none focus:border-primary-30 focus:border-b-2 transition-colors"
+              autoComplete="off"
+              {...register("status", {
+                required: {
+                  value: true,
+                  message: "This field is required",
+                },
+              })}
+            />
+            {errors.status && (
+              <span className="text-sm text-red-20 text-start">
+                {errors.studyLeave?.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label
+              className="text-gray-30 text-sm text-start"
+              htmlFor="studyLeave"
+            >
+              {t("application_3_msg28")}
+            </label>
+            <div className="flex items-center gap-2">
+              <label htmlFor="" className="text-gray-30 text-sm text-start">
+                %
+              </label>
+              <input
+                type="text"
+                className="border-b p-2 border-b-primary-10 rounded-md focus:outline-none focus:border-primary-30 focus:border-b-2 transition-colors"
+                autoComplete="off"
+                {...register("studyLeave", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                })}
+              />
+              <label htmlFor="" className="text-gray-30 text-sm text-start">
+                US$
+              </label>
+              <input
+                type="text"
+                className="border-b p-2 border-b-primary-10 rounded-md focus:outline-none focus:border-primary-30 focus:border-b-2 transition-colors"
+                autoComplete="off"
+                {...register("studyLeave", {
+                  required: {
+                    value: true,
+                    message: "This field is required",
+                  },
+                })}
+              />
+            </div>
+            {errors.studyLeave && (
+              <span className="text-sm text-red-20 text-start">
+                {errors.studyLeave?.message}
+              </span>
+            )}
           </div>
           <button type="submit" className="btn self-center mt-4">
             {t("application_2_msg6")}
